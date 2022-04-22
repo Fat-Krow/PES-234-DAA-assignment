3540026b9289b9b93b54f511116935083dd72d7f
Nytik B <iamnytik@gmail.com>
2022-04-22 21:10:09 +0530

    finished implementing quick sort it was hard

--- PES1UG20CS000.c
+++ PES1UG20CS000.c
@@ -4,8 +4,9 @@ static void reverse_graph (int n,const connection_t connection[n][n],connection_
 static void dfs (int i,int n,const connection_t connection[n][n],int airports[n],int stack[n],int* top);
 int count_ones(int n,int arr[n]);
 int path_find(int n,int  dest ,const connection_t connection[n][n] ,int k,int sour,int *visited,int curr_path_length);
-
-
+void swap(airport_t *a, airport_t *b);
+int partition(int n,airport_t array[n], int (*predicate_func)(const airport_t *, const airport_t *), int low, int high);
+void quickSort(int n,airport_t array[n], int (*predicate_func)(const airport_t *, const airport_t *), int low, int high);
 // ANY STATIC FUNCTIONS ARE UP HERE
 static void  reverse_graph(int n, const connection_t connection[n][n],connection_t rev[n][n])
 {   //connection_t copy;
@@ -27,7 +28,7 @@ static void dfs (int i,int n,const connection_t connection[n][n],int airports[n]
 
     for(int j=0; j<n ;j++)
     {   //edge=connection[i][j];
-        if ( (connection[i][j].distance!=INT_MAX)&&(connection[i][j].time!=INT_MAX))//error
+        if ( (connection[i][j].distance!=INT_MAX)&&(connection[i][j].time!=INT_MAX))
         {   if (airports[j]==0)
                 {dfs(j,n,connection,airports,stack,top);}
         }
@@ -64,7 +65,7 @@ int path_find(int n,int  dest ,const connection_t connection[n][n] ,int k,int sr
 
         for(int j=0;j<n;j++)
         {
-            if(visited[j]!=1 && connection[src][j].distance!=INT_MAX && connection[src][j].time!=INT_MAX )
+            if (visited[j] != 1 && connection[src][j].distance != INT_MAX && connection[src][j].time != INT_MAX)
             {
                int flag= path_find(n,dest,connection,k,j,visited,curr_path_length+1);
 
@@ -79,6 +80,84 @@ int path_find(int n,int  dest ,const connection_t connection[n][n] ,int k,int sr
         }
         return 0;//from this node there is no unvisted node
 
+}
+// function to swap elements
+void swap(airport_t *a, airport_t *b)
+{
+  airport_t t = *a;
+  *a = *b;
+  *b = t;
+}
+// function to find the partition position
+int partition(int n,airport_t array[n], int (*predicate_func)(const airport_t *, const airport_t *), int low, int high)
+{
+
+  // select the rightmost element as pivot
+  airport_t pivot = array[high];
+
+  // pointer for greater element
+  int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (int j = low; j < high; j++)
+  {
+    if (predicate_func(&array[j],&pivot)) 
+    {
+
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+
+  // return the partition point
+  return (i + 1);
+}
+void quickSort(int n,airport_t array[n], int (*predicate_func)(const airport_t *, const airport_t *), int low, int high) 
+{
+  
+  if ((low<high)&&predicate_func(&array[low],&array[high])) //this is for ascending 
+  {
+    
+    // find the pivot element such that n-1
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    int pi = partition(n,array,predicate_func, low, high);
+
+    // recursive call on the left of pivot
+        
+    quickSort(n,array,predicate_func, low, pi - 1);
+            
+
+    // recursive call on the right of pivot
+    quickSort(n,array,predicate_func, pi + 1, high);
+         
+  }
+  else if ((low<high)&&predicate_func(&array[high],&array[low]))//this for descending patterns
+  {
+        
+    // find the pivot element such that n-1
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    int pi = partition(n,array,predicate_func, low, high);
+
+    // recursive call on the left of pivot
+        
+    quickSort(n,array,predicate_func, low, pi - 1);
+            
+
+    // recursive call on the right of pivot
+    quickSort(n,array,predicate_func, pi + 1, high); 
+         
+
+  }
 }
 
 // YOUR SOLUTIONS BELOW
@@ -141,15 +220,44 @@ int q2(const airport_t *src, const airport_t *dest, int n, int k,const connectio
 
 int q3(const airport_t *src, int n, const connection_t connections[n][n])
 {
+//Finding a loop in a graph
+    int adjacent[n];
+    int end=0;
+    for (int j = 0; j < n; j++)
+    {
+        if((connections[src->num_id][j].distance!=INT_MAX) && (connections[src->num_id][j].time!= INT_MAX))
+        {
+            adjacent[end]=j;/* we are collecting all the airports adjacent  to  the source's
+                            */
+            end=end+1;
+        }
+    }
+    int visited[n];
 
 
-
+    for (int i = 0; i < end; i++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            visited[i]=0;
+        }
+        if(path_find(n,src->num_id,connections,n,adjacent[0],visited,0))
+        {
+            return 1;
+        }
+    }
+    //if we can't find a path from anyone of  the adjacent-to-src nodes
+    //to src we come here
     return 0;
 }
 
 void q4(int n, int (*predicate_func)(const airport_t *, const airport_t *),
         airport_t airport_list[n])
 {
+
+    quickSort(n,airport_list,predicate_func,0,n-1);//IT WORKED!!!!!!!!
+    //yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayy!
+
 }
 
 pair_t q5(int n, airport_t airports[n])
